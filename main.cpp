2077bf136af8aaee054d99f78e3173873677762f
FSund <filipsu@fys.uio.no>
2013-12-02 09:44:06 +0100

    Rewrote program to use std::vector instead of armadillo vectors/matrices

--- main.cpp
+++ main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
-#include <armadillo>
+#include <vector>
 #include <fstream>
 
 using namespace std;
-using namespace arma;
+
+typedef unsigned int uint;
 
 void writeLammpsFile() {
-    ivec atomTypes;
-    atomTypes << 14 << 8 << 14; // Silicon, oxygen, silicon
-    cout << atomTypes << endl;
-    mat positions;
-    positions << 1.0 << 2.5 << -1.5 << endr
-              << 4.0 << -5.0 << 6.0 << endr
-              << 7.0 << 2.0 << -3.0 << endr;
-    cout << positions << endl;
+    vector<uint> atomTypes = {14, 8, 14};
+    cout << atomTypes[0] << endl;
+
+    vector<vector<double> > positions(3);
+    positions[0] = {1.0,  2.5, -1.5};
+    positions[1] = {4.0, -5.0,  6.0};
+    positions[2] = {7.0,  2.0, -3.0};
+    cout << positions[1][1] << endl;
+
     ofstream lammpsFile("outfile.lmp", ios::out | ios::binary);
+
     // Set up data for the timestep of this file
     int currentTimeStep = 0;
-    int nAtomsTotal = positions.n_rows;
+    int nAtomsTotal = positions.size();
+
     // The system boundaries
     double xMin = -10.0;
     double xMax = 10.0;
@@ -25,15 +29,18 @@ void writeLammpsFile() {
     double yMax = 10.0;
     double zMin = -10.0;
     double zMax = 10.0;
+
     // Shearing is zero unless the system boundaries are sheared (yes that's "sheared",
     // not "shared")
     double xShear = 0.0;
     double yShear = 0.0;
     double zShear = 0.0;
+
     // nColumns is the number of data types you want to write. In our case we want to
     // write four - the atom type and the x, y and z components of the position.
     // If you want velocities, forces, etc., just add more columns and write more data.
     int nColumns = 1 + 3;
+
     // We could divide the data into chunks by the LAMMPS file format, but we don't - i.e. only
     // use one chunk. The chunk length is then the same as the number of atoms times the number
     // of columns.
@@ -57,15 +64,16 @@ void writeLammpsFile() {
     lammpsFile.write(reinterpret_cast<const char*>(&chunkLength), sizeof(int));
 
     // Write all the data for each atom to file
-    for(uint i = 0; i < positions.n_rows; i++) {
+    for(uint i = 0; i < positions.size(); i++) {
         // IMPORTANT: Even atom numbers are usually integers, they must be written
         // as double according to the LAMMPS standard.
-        double atomType = atomTypes(i);
+        double atomType = atomTypes[i];
         lammpsFile.write(reinterpret_cast<const char*>(&atomType), sizeof(double));
+
         // Write the x, y and z-components
-        lammpsFile.write(reinterpret_cast<const char*>(&positions(i,0)), sizeof(double));
-        lammpsFile.write(reinterpret_cast<const char*>(&positions(i,1)), sizeof(double));
-        lammpsFile.write(reinterpret_cast<const char*>(&positions(i,2)), sizeof(double));
+        lammpsFile.write(reinterpret_cast<const char*>(&positions[i][0]), sizeof(double));
+        lammpsFile.write(reinterpret_cast<const char*>(&positions[i][1]), sizeof(double));
+        lammpsFile.write(reinterpret_cast<const char*>(&positions[i][2]), sizeof(double));
     }
     lammpsFile.close();
 }
